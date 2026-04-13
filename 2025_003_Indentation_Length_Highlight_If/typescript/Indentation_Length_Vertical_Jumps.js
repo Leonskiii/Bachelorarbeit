f92d670c0b68031dfcf0b21e0e69f17651d9360d
Leon <splehaus@uni-due.de>
2026-04-13 17:34:07 +0200

    Neue Experimente

--- 2025_003_Indentation_Length_Highlight_If/typescript/Indentation_Length_Vertical_Jumps.js
+++ 2025_003_Indentation_Length_Highlight_If/typescript/Indentation_Length_Vertical_Jumps.js
@@ -1,25 +1,12 @@
-import {BROWSER_EXPERIMENT} from "../../N_of_1_Experiments/modules/Experimentation/Browser_Output_Writer.js";
-import {
-    alternatives,
-    Experiment_Output_Writer, keys, random_array_element, random_integer_up_to_excluding, Reaction_Time,
-    SET_SEED, Standard_Post_Questionnaire
-} from "../../N_of_1_Experiments/modules/Experimentation/Experimentation.js";
-import {Task} from "../../N_of_1_Experiments/modules/Experimentation/Task.js";
-import {generate_If_Statement, Nested_Ifs, Term} from "./Generate_Code.js";
-import {convert_string_to_html_string} from "../../N_of_1_Experiments/modules/utils/Utils.js";
-import {
-    finish_pages,
-    intro_pages,
-    pre_run_experiment_instructions,
-    pre_run_training_instructions
-} from "./Indentation_Length_Vertical_Jumps_Text.js";
-import {Nouns} from "../../N_of_1_Experiments/modules/Words/Nouns.js";
-
+import { BROWSER_EXPERIMENT } from "../../N_of_1_Experiments/modules/Experimentation/Browser_Output_Writer.js";
+import { keys, Reaction_Time, SET_SEED, Standard_Post_Questionnaire } from "../../N_of_1_Experiments/modules/Experimentation/Experimentation.js";
+import { generate_If_Statement, Nested_Ifs } from "./Generate_Code.js";
+import { convert_string_to_html_string } from "../../N_of_1_Experiments/modules/utils/Utils.js";
+import { finish_pages, intro_pages, pre_run_experiment_instructions, pre_run_training_instructions } from "./Indentation_Length_Vertical_Jumps_Text.js";
+import { Nouns } from "../../N_of_1_Experiments/modules/Words/Nouns.js";
 let SEED = "42";
-
 SET_SEED(SEED);
-
-export function set_if_conditions_on_nous_each_starting_with_different_letter(if_statement: Term) {
+export function set_if_conditions_on_nous_each_starting_with_different_letter(if_statement) {
     let starting_letter = [];
     let nouns = new Nouns();
     while (if_statement instanceof Nested_Ifs) {
@@ -32,74 +19,52 @@ export function set_if_conditions_on_nous_each_starting_with_different_letter(if
         if_statement = if_statement.then_branch;
     }
 }
-
-let experiment_configuration_function = (writer: Experiment_Output_Writer) => {
+let experiment_configuration_function = (writer) => {
     return {
-
         experiment_name: "Indentation_Length_Vertical_Jumps",
         seed: SEED,
         introduction_pages: [writer.string_page_command(intro_pages())],
         pre_run_training_instructions: writer.string_page_command(pre_run_training_instructions()),
         pre_run_experiment_instructions: writer.string_page_command(pre_run_experiment_instructions()),
         post_questionnaire: Standard_Post_Questionnaire(),
-        training_configuration: {can_be_cancelled: true, can_be_repeated: true},
+        training_configuration: { can_be_cancelled: true, can_be_repeated: true },
         finish_pages: [writer.string_page_command(finish_pages())],
-
         layout: [
-            {variable: "Length", treatments: ["2", "4", "6", "8"]},
-            {variable: "Level", treatments: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]},
-            {variable: "Distance_from_Center", treatments: ["_computed_"]}
+            { variable: "Length", treatments: ["2", "4", "6", "8"] },
+            { variable: "Level", treatments: ["1", "2", "3", "4", "5", "6", "7", "8", "9"] },
+            { variable: "Distance_from_Center", treatments: ["_computed_"] }
         ],
-
         repetitions: 5,
-
         measurement: Reaction_Time(keys(["1"])),
-
-        task_configuration: (t: Task) => {
+        task_configuration: (t) => {
             let center = 5;
-
             let nesting_depth = center * 2 - 1;
-
             let length = parseInt(t.treatment_value("Length"));
             let level = parseInt(t.treatment_value("Level"));
             let target_expression_number = parseInt(t.treatment_value("Level"));
-
             t.set_computed_variable_value("Distance_from_Center", Math.abs(center - level).toString());
-
             let if_statement = generate_If_Statement(length, nesting_depth);
             set_if_conditions_on_nous_each_starting_with_different_letter(if_statement);
             let target_condition_string = if_statement.target_condition_string(target_expression_number);
-
             let html_string = convert_string_to_html_string(if_statement.print_string(length));
             let correct_answer = if_statement.return_string(target_expression_number);
-
-            console.log(html_string);
-
-            let target_return_value = if_statement.target_return_value(target_expression_number);
-
-            html_string = html_string.replace("}&nbsp;" + target_return_value + "<br/>",
-                "<span style=\"background-color:red\">}&nbsp;" + target_return_value + "</span><br/>");
-
+            html_string = html_string.replace("if(" + target_condition_string + ")&nbsp;{", "<span style=\"background-color:red\">if(" + target_condition_string + ")&nbsp;{</span>");
             t.expected_answer = correct_answer;
-
             t.do_print_task = () => {
                 writer.clear_stage();
                 writer.print_string_on_stage("<div class='sourcecode'>" + html_string + "</div>");
             };
-
             t.accepts_answer = (s) => {
                 return true;
-            }
-
+            };
             t.do_print_after_task_information = () => {
                 writer.clear_stage();
-                writer.print_error_string_on_stage(writer.convert_string_to_html_string(
-                    "The corresponding if-condition was: " + target_condition_string + "\n\n" +
-                    "In case you feel not concentrated enough, make a short break.\n\n" +
+                writer.print_error_string_on_stage(writer.convert_string_to_html_string("The correct answer was: " + t.expected_answer + "\n\n" +
+                    "In case, you feel not concentrated enough, make a short break.\n\n" +
                     "Press [Enter] to go on. "));
-            }
+            };
         }
-    }
+    };
 };
-
 BROWSER_EXPERIMENT(experiment_configuration_function);
+//# sourceMappingURL=Indentation_Length_Vertical_Jumps.js.map
